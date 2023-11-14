cb0ce0105134a81f38f5feba64e855333757d1b4
hasanatay <hasan@dkoda.xyz>
2023-11-14 18:51:35 -0500

    Gas report

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,30 @@
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@openzeppelin/hardhat-upgrades";
 
-
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL || '',
+      accounts: process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : [],
+    }
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    token: "ETH"
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
 
 export default config;
