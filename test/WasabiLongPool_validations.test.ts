29c390385eb6808a1277ef11db672f6ebefc6f52
hasanatay <hasan@dkoda.xyz>
2024-09-04 15:25:48 -0400

    Check the position hash

--- test/WasabiLongPool_validations.test.ts
+++ test/WasabiLongPool_validations.test.ts
@@ -57,7 +57,7 @@ describe("WasabiLongPool - Validations Test", function () {
                 .to.be.rejectedWith("SwapFunctionNeeded", "Cannot open positions without swap functions");
         });
 
-        it("Cannot Reuse Signature", async function () {
+        it.only("Cannot Reuse Signature", async function () {
             const { wasabiLongPool, createSignedClosePositionRequest, sendDefaultOpenPositionRequest, user1 } = await loadFixture(deployLongPoolMockEnvironment);
 
             const {position} = await sendDefaultOpenPositionRequest();
@@ -68,6 +68,9 @@ describe("WasabiLongPool - Validations Test", function () {
 
             await expect(sendDefaultOpenPositionRequest())
                 .to.be.rejectedWith("PositionAlreadyTaken", "Cannot open position if position is it was opened before");
+
+            const positionHash = await wasabiLongPool.read.positions([position.id]);
+            expect(positionHash).to.equal(1n);
         });
 
         it("OrderExpired", async function () {
