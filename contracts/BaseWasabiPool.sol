fda3c790f659c00e06ad8d7f691ac865f66adcdc
WEBthe3rd <bill@dkoda.xyz>
2024-11-18 13:58:44 -0500

    Remove comment

--- contracts/BaseWasabiPool.sol
+++ contracts/BaseWasabiPool.sol
@@ -153,7 +153,6 @@ abstract contract BaseWasabiPool is IWasabiPerps, UUPSUpgradeable, OwnableUpgrad
         if (_token == wethAddress) {
             uint256 total = _closeAmounts.payout + positionFeesToTransfer + _closeAmounts.liquidationFee;
             IWETH wethToken = IWETH(wethAddress);
-            // Ignore UNWRAPPED flag if the trader address is a contract
             if (_payoutType == PayoutType.UNWRAPPED) {
                 if (total > address(this).balance) {
                     wethToken.withdraw(total - address(this).balance);
