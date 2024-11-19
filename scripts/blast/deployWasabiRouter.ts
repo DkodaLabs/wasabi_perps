c029a20de9345731a794ac2e7bcedd67a2f7d305
WEBthe3rd <bill@dkoda.xyz>
2024-11-19 14:18:20 -0500

    Update deploy scripts

--- scripts/blast/deployWasabiRouter.ts
+++ scripts/blast/deployWasabiRouter.ts
@@ -6,13 +6,20 @@ async function main() {
   const longPoolAddress = "0x046299143A880C4d01a318Bc6C9f2C0A5C1Ed355";
   const shortPoolAddress = "0x0301079DaBdC9A2c70b856B2C51ACa02bAc10c3a";
   const perpManagerAddress = "0xff2CDb9cdb79A60A31188FE37Bdc6774107cc268";
+  const weth = "0x4300000000000000000000000000000000000004";
+  const swapRouter = "0x789a11Ced3D407aD7CE4ADf1f7bFAf270b470773";
+  const feeReceiver = "0x5C629f8C0B5368F523C85bFe79d2A8EFB64fB0c8";
+  const withdrawFeeReceiver = "0x97165754beA07D70Ab27C2A9E02728c79ED80d64";
+  const liquidationFeeReceiver = "0xF6336dd76300524Ef382FA9FC861305A37b929b6";
+  const debtControllerAddress = "0xe3f3Dca2Bd68cbD34b58cfc3BCd109998fCce0Ac";
+  const feeBips = 25n;
 
   console.log("1. Deploying BlastRouter...");
   const WasabiRouter = await hre.ethers.getContractFactory("BlastRouter");
   const routerAddress = 
       await hre.upgrades.deployProxy(
           WasabiRouter,
-          [longPoolAddress, shortPoolAddress, perpManagerAddress],
+          [longPoolAddress, shortPoolAddress, weth, perpManagerAddress, swapRouter, withdrawFeeReceiver, feeBips],
           { kind: 'uups' }
       )
       .then(c => c.waitForDeployment())
@@ -25,19 +32,14 @@ async function main() {
 
   console.log("2. Deploying new AddressProvider...");
 
-  const debtControllerAddress = "0xe3f3Dca2Bd68cbD34b58cfc3BCd109998fCce0Ac";
-  const wethAddress = "0x4300000000000000000000000000000000000004";
-  const feeReceiver = "0x5C629f8C0B5368F523C85bFe79d2A8EFB64fB0c8";
-  const liquidationFeeReceiver = "0xF6336dd76300524Ef382FA9FC861305A37b929b6";
-
   const addressProvider = 
     await hre.viem.deployContract(
         "AddressProvider",
-        [debtControllerAddress, routerAddress, feeReceiver, wethAddress, liquidationFeeReceiver]);
+        [debtControllerAddress, routerAddress, feeReceiver, weth, liquidationFeeReceiver]);
   console.log(`AddressProvider deployed to ${addressProvider.address}`);
 
   await delay(10_000);
-  await verifyContract(addressProvider.address, [debtControllerAddress, routerAddress, feeReceiver, wethAddress, liquidationFeeReceiver]);
+  await verifyContract(addressProvider.address, [debtControllerAddress, routerAddress, feeReceiver, weth, liquidationFeeReceiver]);
 }
 
 function delay(ms: number) {
--- scripts/mainnet/deployWasabiRouter.ts
+++ scripts/mainnet/deployWasabiRouter.ts
@@ -6,13 +6,20 @@ async function main() {
   const longPoolAddress = "0x8e0edfd6d15f858adbb41677b82ab64797d5afc0";
   const shortPoolAddress = "0x0fdc7b5ce282763d5372a44b01db65e14830d8ff";
   const perpManagerAddress = "0xc0b01a4f4A4459D5A7E13C2E8566CDe93A010e7D";
+  const debtControllerAddress = "0xFe7B8F5722ac881242e16beBc8Ea0B28c3EE60C2";
+  const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  const swapRouter = "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45";
+  const feeReceiver = "0x5C629f8C0B5368F523C85bFe79d2A8EFB64fB0c8";
+  const withdrawFeeReceiver = "0x97165754beA07D70Ab27C2A9E02728c79ED80d64";
+  const liquidationFeeReceiver = "0xF6336dd76300524Ef382FA9FC861305A37b929b6";
+  const feeBips = 25n;
 
   console.log("1. Deploying WasabiRouter...");
   const WasabiRouter = await hre.ethers.getContractFactory("WasabiRouter");
   const routerAddress = 
       await hre.upgrades.deployProxy(
           WasabiRouter,
-          [longPoolAddress, shortPoolAddress, perpManagerAddress],
+          [longPoolAddress, shortPoolAddress, wethAddress, perpManagerAddress, swapRouter, withdrawFeeReceiver, feeBips],
           { kind: 'uups' }
       )
       .then(c => c.waitForDeployment())
@@ -24,11 +31,6 @@ async function main() {
   await verifyContract(routerAddress, []);
 
   console.log("2. Deploying new AddressProvider...");
-
-  const debtControllerAddress = "0xFe7B8F5722ac881242e16beBc8Ea0B28c3EE60C2";
-  const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-  const feeReceiver = "0x5C629f8C0B5368F523C85bFe79d2A8EFB64fB0c8";
-  const liquidationFeeReceiver = "0xF6336dd76300524Ef382FA9FC861305A37b929b6";
 
   const addressProvider = 
     await hre.viem.deployContract(
