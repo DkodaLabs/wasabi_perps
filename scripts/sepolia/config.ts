b8c32db894b095d7cd030a009cd51827ea02593b
WEBthe3rd <bill@dkoda.xyz>
2025-04-21 13:29:08 -0400

    Add ExactOutSwapper to configs

--- scripts/sepolia/config.ts
+++ scripts/sepolia/config.ts
@@ -9,4 +9,5 @@ export const CONFIG: ChainConfig = {
     weth: "0x6400c43e5dd1f713fd623d92dc64831dd12d3298",
     swapRouter: "0xD0b8934c079c06e0A458A26272EE5238c1c02bCA",
     swapFeeReceiver: "0x97165754beA07D70Ab27C2A9E02728c79ED80d64",
+    exactOutSwapper: "0x"
 }
