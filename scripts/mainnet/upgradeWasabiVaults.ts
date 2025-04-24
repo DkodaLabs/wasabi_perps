b0d5a1588e948e971121194cdf17f2ada5cfc61a
hasanatay <hasan@dkoda.xyz>
2025-04-24 10:09:58 -0400

    mainnet + base vaults

--- scripts/mainnet/upgradeWasabiVaults.ts
+++ scripts/mainnet/upgradeWasabiVaults.ts
@@ -1,45 +1,33 @@
-import { formatEther, parseEther, getAddress } from "viem";
+import { getAddress } from "viem";
 import hre from "hardhat";
 import { verifyContract } from "../../utils/verifyContract";
 
 import WasabiVaults from "./mainnetVaults.json";
 
 async function main() {
-
-  console.log("1. Upgrading WasabiVaults...");
+  console.log("1. Upgrading Vaults...");
+  const manager = "0xc0b01a4f4A4459D5A7E13C2E8566CDe93A010e7D"
   const WasabiVault = await hre.ethers.getContractFactory("WasabiVault");
-
-  const longPoolAddress = "0x8e0edfd6d15f858adbb41677b82ab64797d5afc0";
-  const shortPoolAddress = "0x0fdc7b5ce282763d5372a44b01db65e14830d8ff";
-  const addressProviderAddress = "0x2b04347413918588b81782cc446524354a15ee72";
 
   for (let i = 0; i < WasabiVaults.length; i++) {
     const vault = WasabiVaults[i];
-    let feesToKeep: bigint;
-    if (vault.symbol === "wWETH") {
-      feesToKeep = 640118000000000000n; // TODO: calculate this using the script we prepared
-    } else {
-      feesToKeep = 0n;
-    }
+    console.log(`  Upgrading WasabiVault ${vault.name}...`);
     const address =
       await hre.upgrades.upgradeProxy(
           vault.address,
           WasabiVault,
           {
-            redeployImplementation: i == 0 ? "always" : undefined,
             call: {
-              fn: "migrate",
-              args: [
-                longPoolAddress, 
-                shortPoolAddress,
-                addressProviderAddress,
-                feesToKeep
-              ]
+              fn: "transferOwnership",
+              args: [manager],
             }
           }
       )
       .then(c => c.waitForDeployment())
       .then(c => c.getAddress()).then(getAddress);
+
+    await delay(10_000);
+
     const implAddress = getAddress(await hre.upgrades.erc1967.getImplementationAddress(address));
     console.log(`${i + 1}/${WasabiVaults.length} - WasabiVault ${vault.name} upgraded to ${implAddress}`);
 
