5372ccf718ff49dcc1d03390bb960a1487f55410
WEBthe3rd <bill@dkoda.xyz>
2025-05-23 13:21:47 -0400

    Refactor with triggered cooldown period

--- contracts/vaults/ITimelock.sol
+++ contracts/vaults/ITimelock.sol
@@ -2,18 +2,28 @@
 pragma solidity ^0.8.23;
 
 interface ITimelock {
-    error TimelockNotEnded(); // 0x050f6771
+    error InsufficientCooldown(); // 0x1a731408
+    error InvalidCooldownAmount(); // 0x8e99de94
 
-    event TimelockDurationUpdated(uint256 oldDuration, uint256 newDuration);
+    event CooldownStarted(address indexed account, uint256 amount);
+    event CooldownDurationUpdated(uint256 oldDuration, uint256 newDuration);
+
+    struct Cooldown {
+        uint256 cooldownStart;
+        uint256 amount;
+    }
 
     struct TimelockStorage {
-        uint256 timelockDuration;
-        mapping(address => uint256) timelockStart;
+        uint256 cooldownDuration;
+        mapping(address => Cooldown[]) cooldowns;
+        mapping(address => uint256) nextCooldownIndex;
     }
     
-    function getTimelockEnd(address account) external view returns (uint256);
+    function getCooldowns(address account) external view returns (Cooldown[] memory);
 
-    function getTimelockDuration() external view returns (uint256);
+    function getCooldownDuration() external view returns (uint256);
 
-    function setTimelockDuration(uint256 duration) external;
+    function setCooldownDuration(uint256 duration) external;
+
+    function startCooldown(uint256 amount) external;
 }
--- contracts/vaults/TimelockWasabiVault.sol
+++ contracts/vaults/TimelockWasabiVault.sol
@@ -20,7 +20,7 @@ contract TimelockWasabiVault is WasabiVault, ITimelock {
     /// @param _asset The asset
     /// @param name The name of the vault
     /// @param symbol The symbol of the vault
-    /// @param _timelockDuration The duration of the timelock in seconds
+    /// @param _cooldownDuration The duration of the cooldown period in seconds
     function initialize(
         IWasabiPerps _longPool,
         IWasabiPerps _shortPool,
@@ -29,75 +29,65 @@ contract TimelockWasabiVault is WasabiVault, ITimelock {
         IERC20 _asset,
         string memory name,
         string memory symbol,
-        uint256 _timelockDuration
+        uint256 _cooldownDuration
     ) public virtual initializer {
         __WasabiVault_init(_longPool, _shortPool, _addressProvider, _manager, _asset, name, symbol);
-        _getTimelockStorage().timelockDuration = _timelockDuration;
-        emit TimelockDurationUpdated(0, _timelockDuration);
-    }
-
-    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
-    /*                         MODIFIERS                          */
-    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
-
-    modifier onlyAfterTimelock(address account) {
-        TimelockStorage storage ts = _getTimelockStorage();
-        if (block.timestamp < ts.timelockStart[account] + ts.timelockDuration) {
-            revert TimelockNotEnded();
-        }
-        _;
-    }
-
-    modifier setTimelock(address account) {
-        _getTimelockStorage().timelockStart[account] = block.timestamp;
-        _;
+        _getTimelockStorage().cooldownDuration = _cooldownDuration;
+        emit CooldownDurationUpdated(0, _cooldownDuration);
     }
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                          GETTERS                           */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
-    function getTimelockDuration() external view returns (uint256) {
-        return _getTimelockStorage().timelockDuration;
+    function getCooldownDuration() external view returns (uint256) {
+        return _getTimelockStorage().cooldownDuration;
     }
 
-    function getTimelockEnd(address account) external view returns (uint256) {
-        TimelockStorage storage ts = _getTimelockStorage();
-        return ts.timelockStart[account] + ts.timelockDuration;
+    function getCooldowns(address account) external view returns (Cooldown[] memory) {
+        return _getTimelockStorage().cooldowns[account];
     }
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                           WRITES                           */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
-    /** @dev See {IERC4626-deposit}. */
-    function depositEth(address receiver) public payable override setTimelock(receiver) returns (uint256) {
-        return super.depositEth(receiver);
+    function startCooldown(uint256 amount) external {
+        if (amount == 0) revert InvalidCooldownAmount();
+
+        Cooldown[] storage cooldowns = _getTimelockStorage().cooldowns[msg.sender];
+        uint256 stakedBalance = balanceOf(msg.sender);
+        for (uint256 i; i < cooldowns.length; ) {
+            stakedBalance -= cooldowns[i].amount;
+            unchecked {
+                i++;
+            }
+        }
+        if (stakedBalance < amount) revert InvalidCooldownAmount();
+
+        // Add cooldown to the end of the array
+        // @dev Cooldowns should always remain sorted by cooldownStart
+        cooldowns.push(Cooldown({
+            cooldownStart: block.timestamp,
+            amount: amount
+        }));
+
+        emit CooldownStarted(msg.sender, amount);
     }
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                       ADMIN FUNCTIONS                      */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
-    function setTimelockDuration(uint256 duration) external onlyAdmin {
+    function setCooldownDuration(uint256 duration) external onlyAdmin {
         TimelockStorage storage ts = _getTimelockStorage();
-        emit TimelockDurationUpdated(ts.timelockDuration, duration);
-        ts.timelockDuration = duration;
+        emit CooldownDurationUpdated(ts.cooldownDuration, duration);
+        ts.cooldownDuration = duration;
     }
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                     INTERNAL FUNCTIONS                     */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
-
-    /// @inheritdoc ERC4626Upgradeable
-    function _deposit(
-        address caller,
-        address receiver,
-        uint256 assets,
-        uint256 shares
-    ) internal override setTimelock(receiver) {
-        super._deposit(caller, receiver, assets, shares);
-    }
 
     /// @inheritdoc ERC4626Upgradeable
     function _withdraw(
@@ -106,8 +96,48 @@ contract TimelockWasabiVault is WasabiVault, ITimelock {
         address owner,
         uint256 assets,
         uint256 shares
-    ) internal override onlyAfterTimelock(owner) {
+    ) internal override {
+        _checkAndUpdateCooldowns(owner, shares);
         super._withdraw(caller, receiver, owner, assets, shares);
+    }
+
+    function _checkAndUpdateCooldowns(address owner, uint256 withdrawAmount) internal {
+        TimelockStorage storage ts = _getTimelockStorage();
+        uint256 cooldownDuration = ts.cooldownDuration;
+        // If cooldown duration is 0, withdrawals are not timelocked
+        if (cooldownDuration == 0) return;
+
+        Cooldown[] storage cooldowns = ts.cooldowns[owner];
+        uint256 cooldownsLength = cooldowns.length;
+        uint256 nextCooldownIndex = ts.nextCooldownIndex[owner];
+        // If there are no cooldowns or the next cooldown index is out of bounds, the cooldowns are insufficient
+        if (cooldownsLength == 0 || nextCooldownIndex >= cooldownsLength) revert InsufficientCooldown();
+
+        for (uint256 i = nextCooldownIndex; i < cooldownsLength; ) {
+            Cooldown memory cooldown = cooldowns[i];
+            // Break if the cooldown has not ended, since all subsequent cooldowns will also not have ended
+            if (block.timestamp < cooldown.cooldownStart + cooldownDuration) break;
+            if (withdrawAmount >= cooldown.amount) {
+                // Completely withdraw this cooldown
+                withdrawAmount -= cooldown.amount;
+                // Set values to 0 without shifting or changing array length
+                delete cooldowns[i];
+                // Skip this cooldown in future iterations
+                ts.nextCooldownIndex[owner] = i + 1;
+                if (withdrawAmount == 0) break;
+                unchecked {
+                    i++;
+                }
+            } else {
+                // Withdraw partial amount from this cooldown
+                cooldowns[i].amount -= withdrawAmount;
+                withdrawAmount = 0;
+                break;
+            }
+        }
+
+        // If there is still a remaining amount to withdraw, the cooldowns are insufficient
+        if (withdrawAmount > 0) revert InsufficientCooldown();
     }
 
     function _getTimelockStorage() internal pure returns (TimelockStorage storage $) {
