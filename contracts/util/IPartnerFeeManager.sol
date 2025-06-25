7d1ab57949b31805b74280b3457bb54bb31ee81b
WEBthe3rd <bill@dkoda.xyz>
2025-06-25 12:59:35 -0400

    Initial PartnerFeeManager implementation

---
+++ contracts/util/IPartnerFeeManager.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+interface IPartnerFeeManager {
+    error AddressNotPartner();
+    error CallerNotPool();
+    error InvalidFeeShareBips();
+
+    event FeesAccrued(address indexed partner, address indexed feeToken, uint256 amount);
+    event FeesClaimed(address indexed partner, address indexed feeToken, uint256 amount);
+
+    /// @dev Returns true if the given address is a partner
+    /// @param partner the address to check 
+    /// @return true if the given address is a partner, false otherwise
+    function isPartner(address partner) external view returns (bool);
+
+    /// @dev Returns the fee share in basis points
+    /// @return the fee share in basis points
+    function getFeeShareBips() external view returns (uint256);
+
+    /// @dev Returns the accrued fees for the given partner and fee token
+    /// @param partner the partner to get accrued fees for
+    /// @param feeToken the fee token to get accrued fees for
+    /// @return the accrued fees for the given partner and fee token
+    function getAccruedFees(address partner, address feeToken) external view returns (uint256);
+
+    /// @dev Accrues fees for the given partner and fee token
+    /// @param partner the partner to accrue fees to
+    /// @param feeToken the fee token to accrue fees in
+    /// @param totalFees the total fees for the trade
+    /// @return the amount of fees accrued to the partner
+    function accrueFees(address partner, address feeToken, uint256 totalFees) external returns (uint256);
+
+    /// @dev Claims fees for the given fee tokens if the caller is a partner
+    /// @param feeTokens the fee tokens to claim
+    function claimFees(address[] calldata feeTokens) external;
+
+    /// @dev Sets isPartner to true for the given partner
+    /// @param partner the partner to add
+    function addPartner(address partner) external;
+
+    /// @dev Sets isPartner to false for the given partner
+    /// @param partner the partner to remove
+    function removePartner(address partner) external;
+
+    /// @dev Sets the partner fee share percentage
+    /// @param feeShareBips the fee share in basis points
+    function setFeeShareBips(uint256 feeShareBips) external;
+
+    /// @dev Adds fees to the accrued fees for the given partner and fee token
+    /// @param partner the partner to accrue fees to
+    /// @param feeToken the fee token to accrue fees in
+    /// @param amount the amount of fees to accrue to the partner
+    function adminAddFees(address partner, address feeToken, uint256 amount) external;
+}
---
+++ contracts/util/PartnerFeeManager.sol
@@ -0,0 +1,140 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+
+import {IPartnerFeeManager} from "./IPartnerFeeManager.sol";
+import {PerpManager} from "../admin/PerpManager.sol";
+
+contract PartnerFeeManager is UUPSUpgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeable, IPartnerFeeManager {
+    using SafeERC20 for IERC20;
+    using Math for uint256;
+
+    uint256 private constant FEE_DENOMINATOR = 10000;
+    
+    address private _longPool;
+    address private _shortPool;
+    uint256 private _feeShareBips;
+    mapping(address => bool) private _isPartner;
+    mapping(address => mapping(address => uint256)) private _accruedFees;
+
+    /// @dev Checks if the caller is an admin
+    modifier onlyAdmin() {
+        _getManager().isAdmin(msg.sender);
+        _;
+    }
+
+    /// @dev Checks if the caller is one of the pool contracts
+    modifier onlyPool() {
+        if (msg.sender != address(_shortPool)) {
+            // Nested checks save a little gas compared to using &&
+            if (msg.sender != address(_longPool)) revert CallerNotPool();
+        }
+        _;
+    }
+
+    /// @dev Checks if the given address is a partner
+    modifier onlyPartner(address partner) {
+        if (!_isPartner[partner]) revert AddressNotPartner();
+        _;
+    }
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    /// @dev Initializes the contract
+    /// @param manager the PerpManager contract address
+    /// @param longPool the long pool contract address
+    /// @param shortPool the short pool contract address
+    /// @param feeShareBips the fee share in basis points
+    function initialize(address manager, address longPool, address shortPool, uint256 feeShareBips) external initializer {
+        __UUPSUpgradeable_init();
+        __ReentrancyGuard_init();
+        __Ownable_init(manager);
+        _longPool = longPool;
+        _shortPool = shortPool;
+        _feeShareBips = feeShareBips;
+    }
+
+    /// @inheritdoc IPartnerFeeManager
+    function isPartner(address partner) external view returns (bool) {
+        return _isPartner[partner];
+    }
+
+    /// @inheritdoc IPartnerFeeManager
+    function getFeeShareBips() external view returns (uint256) {
+        return _feeShareBips;
+    }
+
+    /// @inheritdoc IPartnerFeeManager
+    function getAccruedFees(address partner, address feeToken) external view returns (uint256) {
+        return _accruedFees[partner][feeToken];
+    }
+
+    /// @inheritdoc IPartnerFeeManager
+    function accrueFees(
+        address partner, 
+        address feeToken, 
+        uint256 totalFees
+    ) external onlyPool onlyPartner(partner) returns (uint256) {
+        uint256 partnerFees = totalFees.mulDiv(_feeShareBips, FEE_DENOMINATOR);
+        if (partnerFees == 0) return 0;
+
+        IERC20(feeToken).safeTransferFrom(msg.sender, address(this), partnerFees);
+        _accruedFees[partner][feeToken] += partnerFees;
+
+        emit FeesAccrued(partner, feeToken, partnerFees);
+        return partnerFees;
+    }
+
+    /// @inheritdoc IPartnerFeeManager
+    function claimFees(address[] calldata feeTokens) external nonReentrant onlyPartner(msg.sender) {
+        uint256 length = feeTokens.length;
+        for (uint256 i = 0; i < length; i++) {
+            address feeToken = feeTokens[i];
+            uint256 amount = _accruedFees[msg.sender][feeToken];
+            if (amount == 0) continue;
+            _accruedFees[msg.sender][feeToken] = 0;
+            IERC20(feeToken).safeTransfer(msg.sender, amount);
+            emit FeesClaimed(msg.sender, feeToken, amount);
+        }
+    }
+
+    /// @inheritdoc IPartnerFeeManager
+    function addPartner(address partner) external onlyAdmin {
+        _isPartner[partner] = true;
+    }
+
+    /// @inheritdoc IPartnerFeeManager
+    function removePartner(address partner) external onlyAdmin {
+        _isPartner[partner] = false;
+    }
+
+    /// @inheritdoc IPartnerFeeManager
+    function setFeeShareBips(uint256 feeShareBips) external onlyAdmin {
+        if (feeShareBips > FEE_DENOMINATOR) revert InvalidFeeShareBips();
+        _feeShareBips = feeShareBips;
+    }
+
+    /// @inheritdoc IPartnerFeeManager
+    function adminAddFees(address partner, address feeToken, uint256 amount) external onlyAdmin onlyPartner(partner) {
+        IERC20(feeToken).safeTransferFrom(msg.sender, address(this), amount);
+        _accruedFees[partner][feeToken] += amount;
+        emit FeesAccrued(partner, feeToken, amount);
+    }
+
+    /// @inheritdoc UUPSUpgradeable
+    function _authorizeUpgrade(address) internal view override onlyAdmin {}
+
+    /// @dev returns the manager of the contract
+    function _getManager() internal view returns (PerpManager) {
+        return PerpManager(owner());
+    }
+}
