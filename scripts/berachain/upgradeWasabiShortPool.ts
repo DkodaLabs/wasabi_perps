c547f468a737ebcae28f116093710527dde9e2c6
hasanatay <hasan@dkoda.xyz>
2025-07-10 12:33:20 -0400

    upgrades for regular interest payment

--- scripts/berachain/upgradeWasabiShortPool.ts
+++ scripts/berachain/upgradeWasabiShortPool.ts
@@ -4,13 +4,12 @@ import { verifyContract } from "../../utils/verifyContract";
 import { CONFIG } from "./config";
 
 async function main() {
-  const currentAddress = getAddress(CONFIG.shortPool); // TODO: Update this address before upgrading
   const WasabiShortPool = await hre.ethers.getContractFactory("WasabiShortPool");
 
   console.log("1. Upgrading WasabiShortPool...");
   const address =
     await hre.upgrades.upgradeProxy(
-      currentAddress,
+      CONFIG.shortPool,
       WasabiShortPool,
       {
         redeployImplementation: 'always',
