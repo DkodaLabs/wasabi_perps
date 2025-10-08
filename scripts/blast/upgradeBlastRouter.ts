bd9b9559b244c36c340f44582ca1181b734621e1
WEBthe3rd <bill@dkoda.xyz>
2025-10-08 15:29:36 -0400

    Upgrade scripts w/ `setWhitelistedFunctionSelectors`

--- scripts/blast/upgradeBlastRouter.ts
+++ scripts/blast/upgradeBlastRouter.ts
@@ -11,7 +11,32 @@ async function main() {
   const address =
     await hre.upgrades.upgradeProxy(
       CONFIG.wasabiRouter,
-      BlastRouter
+      BlastRouter,
+      {
+        call: {
+          fn: "setWhitelistedFunctionSelectors",
+          args: [
+            [
+              // Uniswap
+              "0xac9650d8", // multicall(bytes[])
+              "0x472b43f3", // swapExactTokensForTokens(uint256,uint256,address[],address)
+              "0x42712a67", // swapTokensForExactTokens(uint256,uint256,address[],address)
+              "0x04e45aaf", // exactInputSingle((address,address,uint24,address,uint256,uint256,uint160))
+              "0xb858183f", // exactInput((bytes,address,uint256,uint256))
+              "0x5023b4df", // exactOutputSingle((address,address,uint24,address,uint256,uint256,uint160))
+              "0x09b81346", // exactOutput((bytes,address,uint256,uint256))
+              // OpenOcean
+              "0x90411a32", // swap(address,(address,address,address,address,uint256,uint256,uint256,uint256,address,bytes),(uint256,uint256,uint256,bytes)[])
+              // Odos
+              "0x83bd37f9", // swapCompact()
+              "0x30f80b4c", // swap((address,uint256,address,address,uint256,uint256,address),bytes,address,(uint64,uint64,address))
+              // KyberSwap
+              "0x93eca257", // swap((address,address,bytes,SwapDescriptionV2,bytes))
+            ], 
+            true
+          ]
+        }
+      }
     )
     .then(c => c.waitForDeployment())
     .then(c => c.getAddress()).then(getAddress);
