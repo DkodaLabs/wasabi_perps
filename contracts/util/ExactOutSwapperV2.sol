f68ca5ed8823d657f19cd7d7c8d303f884f89606
WEBthe3rd <bill@dkoda.xyz>
2025-11-11 14:00:39 -0500

    ExactOutSwapperV2

---
+++ contracts/util/ExactOutSwapperV2.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+
+import {IExactOutSwapperV2} from "./IExactOutSwapperV2.sol";
+import {PerpManager} from "../admin/PerpManager.sol";
+
+contract ExactOutSwapperV2 is IExactOutSwapperV2, UUPSUpgradeable, OwnableUpgradeable {
+    using Address for address;
+    using Address for address payable;
+    using SafeERC20 for IERC20;
+
+    uint256 private constant DEFAULT_BUYBACK_DISCOUNT_BIPS = 100; // 1%
+    uint256 private constant BPS_DENOMINATOR = 10000;
+
+    mapping(address => uint256) public buybackDiscountBips;
+    address public longPool;
+    address public shortPool;
+
+    /// @dev Checks if the caller is an admin
+    modifier onlyAdmin() {
+        _getManager().isAdmin(msg.sender);
+        _;
+    }
+
+    /// @dev Checks if the caller is one of the pool contracts
+    modifier onlyPool() {
+        if (msg.sender != shortPool) {
+            if (msg.sender != longPool) revert CallerNotPool();
+        }
+        _;
+    }
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address _manager, address _longPool, address _shortPool) external initializer {
+        __Ownable_init(_manager);
+        __UUPSUpgradeable_init();
+        longPool = _longPool;
+        shortPool = _shortPool;
+    }
+
+    /// @inheritdoc IExactOutSwapperV2
+    function swapExactOut(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountInMax,
+        uint256 amountOut,
+        address swapRouter,
+        bytes calldata swapCalldata
+    ) external onlyPool {
+        // 1. Take amountInMax of tokenIn from the caller
+        IERC20(tokenIn).safeTransferFrom(msg.sender, address(this), amountInMax);
+        uint256 outBalanceBefore = IERC20(tokenOut).balanceOf(address(this));
+        uint256 inBalanceBefore = IERC20(tokenIn).balanceOf(address(this));
+
+        // 2. execute swapCalldata: amountInMax of tokenIn -> amountOutFromSwap of tokenOut
+        IERC20(tokenIn).forceApprove(swapRouter, amountInMax);
+        swapRouter.functionCall(swapCalldata);
+        uint256 outBalanceAfter = IERC20(tokenOut).balanceOf(address(this));
+        uint256 amountOutFromSwap = outBalanceAfter - outBalanceBefore;
+        if (amountOutFromSwap < amountOut) {
+            revert InsufficientAmountOutReceived();
+        }
+        uint256 inBalanceAfter = IERC20(tokenIn).balanceOf(address(this));
+        uint256 amountInFromSwap = inBalanceBefore - inBalanceAfter;
+        if (amountInFromSwap != amountInMax) {
+            revert InsufficientAmountInSpent();
+        }
+
+        // 3. compute excess: amountOutFromSwap - amountOut = excess
+        uint256 excess = amountOutFromSwap - amountOut;
+        if (excess > 0) {
+            // 4. compute buyback: excess * amountInFromSwap / amountOutFromSwap * (10000 - discountBips) / 10000 = buybackAmount
+            uint256 buybackDiscount = buybackDiscountBips[tokenOut] != 0 ? buybackDiscountBips[tokenOut] : DEFAULT_BUYBACK_DISCOUNT_BIPS;
+            uint256 buybackAmount = excess * amountInFromSwap * (BPS_DENOMINATOR - buybackDiscount) / BPS_DENOMINATOR / amountOutFromSwap;
+            if (buybackAmount > inBalanceAfter) {
+                revert InsufficientTokenBalance();
+            }
+            if (buybackAmount > 0) {
+                // 5. send buyback of tokenIn back to caller
+                IERC20(tokenIn).safeTransfer(msg.sender, buybackAmount);
+            }
+        }
+
+        // 6. send amountOut of tokenOut
+        IERC20(tokenOut).safeTransfer(msg.sender, amountOut);
+    }
+
+    /// @inheritdoc IExactOutSwapperV2
+    function sellExistingTokens(
+        address token,
+        uint256 amount,
+        address swapRouter,
+        bytes calldata swapCalldata
+    ) external onlyAdmin {
+        IERC20(token).forceApprove(swapRouter, amount);
+        swapRouter.functionCall(swapCalldata);
+    }
+
+    /// @inheritdoc IExactOutSwapperV2
+    function setBuybackDiscountBips(
+        address token,
+        uint256 discountBips
+    ) external onlyAdmin {
+        buybackDiscountBips[token] = discountBips;
+    }
+
+    /// @inheritdoc UUPSUpgradeable
+    function _authorizeUpgrade(address) internal view override onlyAdmin {}
+
+    /// @dev returns the manager of the contract
+    function _getManager() internal view returns (PerpManager) {
+        return PerpManager(owner());
+    }
+}
---
+++ contracts/util/IExactOutSwapperV2.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+interface IExactOutSwapperV2 {
+    error InsufficientAmountOutReceived(); // 0x545831f6
+    error InsufficientAmountInSpent(); // 0x9982532c
+    error InsufficientTokenBalance(); // 0xe4455cae
+    error CallerNotPool(); // 0xe9211597
+
+    event ExcessTokensPurchased(
+        address tokenBought,
+        uint256 amountBought,
+        address tokenSold,
+        uint256 amountSold
+    );
+
+    /// @dev Swaps amountInMax of tokenIn and buys any excess amount of tokenOut, returning amountOut of tokenOut plus some amount of tokenIn to the caller.
+    /// @param tokenIn The address of the input token
+    /// @param tokenOut The address of the output token
+    /// @param amountInMax The maximum amount of input tokens to spend
+    /// @param amountOut The amount of output tokens to receive
+    /// @param swapRouter The address of the swap router
+    /// @param swapCalldata The calldata for the swap
+    function swapExactOut(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountInMax,
+        uint256 amountOut,
+        address swapRouter,
+        bytes calldata swapCalldata
+    ) external;
+
+    /// @dev Called by the admin to sell tokens that were purchased as excess from a swap.
+    /// @param token The address of the token to sell
+    /// @param amount The amount of tokens to sell
+    /// @param swapRouter The address of the swap router
+    /// @param swapCalldata The calldata for the swap
+    function sellExistingTokens(
+        address token,
+        uint256 amount,
+        address swapRouter,
+        bytes calldata swapCalldata
+    ) external;
+
+    /// @dev Called by the admin to set the buyback discount for a token.
+    /// @param token The address of the token to set the buyback discount for
+    /// @param discountBips The buyback discount in basis points
+    function setBuybackDiscountBips(
+        address token,
+        uint256 discountBips
+    ) external;
+}
