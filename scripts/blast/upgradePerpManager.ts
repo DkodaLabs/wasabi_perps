05043c70145d62e244376cb8a684065866615a97
hasanatay <hasan@dkoda.xyz>
2025-12-15 17:28:33 -0500

    Upgrade Blast

--- scripts/blast/upgradePerpManager.ts
+++ scripts/blast/upgradePerpManager.ts
@@ -8,34 +8,13 @@ async function main() {
     const config = CONFIG;
     
     const perpManagerAddress = config.perpManager;
-    const wasabiRouter = config.wasabiRouter;
-    const weth = config.weth;
-    const partnerFeeManager = config.partnerFeeManager;
-    const feeReceiver = config.feeReceiver;
-    const liquidationFeeReceiver = config.liquidationFeeReceiver;
-    const maxApy = 300n;
-
     const PerpManager = await hre.ethers.getContractFactory("BlastPerpManager");
 
     console.log("1. Upgrading PerpManager...");
     const address =
     await hre.upgrades.upgradeProxy(
       perpManagerAddress,
-      PerpManager,
-      {
-        call: {
-            fn: "migrate",
-            args: [
-                wasabiRouter,
-                feeReceiver,
-                weth,
-                liquidationFeeReceiver,
-                zeroAddress,    // stakingAccountFactory
-                partnerFeeManager,
-                maxApy
-            ]
-        }
-      }
+      PerpManager
     )
     .then(c => c.waitForDeployment())
     .then(c => c.getAddress()).then(getAddress);
