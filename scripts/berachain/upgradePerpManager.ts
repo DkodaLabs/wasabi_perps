f5c4ff3d061c49f24d8d17a5d4e8831cccafc44f
hasanatay <hasan@dkoda.xyz>
2025-12-15 17:21:07 -0500

    bera upgrades

--- scripts/berachain/upgradePerpManager.ts
+++ scripts/berachain/upgradePerpManager.ts
@@ -8,13 +8,6 @@ async function main() {
     const config = CONFIG;
     
     const perpManagerAddress = config.perpManager;
-    const wasabiRouter = config.wasabiRouter;
-    const weth = config.weth;
-    const partnerFeeManager = config.partnerFeeManager;
-    const feeReceiver = config.feeReceiver;
-    const stakingAccountFactory = config.stakingAccountFactory || zeroAddress;
-    const liquidationFeeReceiver = config.liquidationFeeReceiver || zeroAddress;
-    const maxApy = 300n;
 
     const PerpManager = await hre.ethers.getContractFactory("PerpManager");
 
@@ -22,21 +15,7 @@ async function main() {
     const address =
     await hre.upgrades.upgradeProxy(
       perpManagerAddress,
-      PerpManager,
-      {
-        call: {
-            fn: "migrate",
-            args: [
-                wasabiRouter,
-                feeReceiver,
-                weth,
-                liquidationFeeReceiver,
-                stakingAccountFactory,
-                partnerFeeManager,
-                maxApy
-            ]
-        }
-      }
+      PerpManager
     )
     .then(c => c.waitForDeployment())
     .then(c => c.getAddress()).then(getAddress);
