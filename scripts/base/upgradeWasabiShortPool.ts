6661594a3a87839ce1cdb66bf256ba2f51d6c036
hasanatay <hasan@dkoda.xyz>
2026-02-25 12:46:33 -0500

    base upgrades

--- scripts/base/upgradeWasabiShortPool.ts
+++ scripts/base/upgradeWasabiShortPool.ts
@@ -19,7 +19,7 @@ async function main() {
     .then(c => c.getAddress()).then(getAddress);
   console.log(`WasabiShortPool upgraded to ${address}`);
 
-  await delay(10_000);
+  await delay(5_000);
   await verifyContract(address);
 }
 
